9947fbe538ea937efef1e31e65ff88089b565cc8
Bevan Arps <bevan.arps@microsoft.com>
2020-05-01 13:42:16 +1200

    Use go idiom for parallel tests

--- hack/generator/pkg/jsonast/createGlobbingRegex_test.go
+++ hack/generator/pkg/jsonast/createGlobbingRegex_test.go
@@ -3,43 +3,56 @@ package jsonast
 import (
 	"testing"
 
-	"github.com/onsi/gomega"
 	. "github.com/onsi/gomega"
 )
 
 func Test_CreateGlobbingRegex_ReturnsExpectedRegex(t *testing.T) {
-	g := NewGomegaWithT(t)
-	CheckReturnsExpectedRegex(g, "*preview", "^.*preview$")
-	CheckReturnsExpectedRegex(g, "*.bak", "^.*\\.bak$")
-	CheckReturnsExpectedRegex(g, "2014*", "^2014.*$")
-	CheckReturnsExpectedRegex(g, "2014-??-??", "^2014-..-..$")
-}
 
-func CheckReturnsExpectedRegex(g *gomega.WithT, globbing string, regex string) {
-	r := createGlobbingRegex(globbing)
-	s := r.String()
-	g.Expect(s).To(Equal(regex))
+	cases := []struct {
+		glob  string
+		regex string
+	}{
+		{"*preview", "^.*preview$"},
+		{"*.bak", "^.*\\.bak$"},
+		{"2014*", "^2014.*$"},
+		{"2014-??-??", "^2014-..-..$"},
+	}
+
+	for _, c := range cases {
+		c := c
+		t.Run(c.glob, func(t *testing.T) {
+			t.Parallel()
+			g := NewGomegaWithT(t)
+			r := createGlobbingRegex(c.glob)
+			s := r.String()
+			g.Expect(s).To(Equal(c.regex))
+		})
+	}
 }
 
 func Test_GlobbingRegex_MatchesExpectedStrings(t *testing.T) {
-	g := NewGomegaWithT(t)
-	CheckMatchFound(g, "*preview", "2020-02-01preview")
-	CheckMatchNotFound(g, "*preview", "2020-02-01")
 
-	CheckMatchFound(g, "2020-*", "2020-02-01")
-	CheckMatchFound(g, "2020-*", "2020-01-01")
-	CheckMatchNotFound(g, "2020-*", "2019-01-01")
-	CheckMatchNotFound(g, "2020-*", "2015-07-01")
-}
+	cases := []struct {
+		regex       string
+		candidate   string
+		shouldMatch bool
+	}{
+		{"*preview", "2020-02-01preview", true},
+		{"*preview", "2020-02-01", false},
+		{"2020-*", "2020-02-01", true},
+		{"2020-*", "2020-01-01", true},
+		{"2020-*", "2019-01-01", false},
+		{"2020-*", "2015-07-01", false},
+	}
 
-func CheckMatchFound(g *gomega.WithT, globbing string, candidate string) {
-	r := createGlobbingRegex(globbing)
-	match := r.MatchString(candidate)
-	g.Expect(match).To(BeTrue())
-}
-
-func CheckMatchNotFound(g *gomega.WithT, globbing string, candidate string) {
-	r := createGlobbingRegex(globbing)
-	match := r.MatchString(candidate)
-	g.Expect(match).To(BeFalse())
+	for _, c := range cases {
+		c := c
+		t.Run(c.candidate, func(t *testing.T) {
+			t.Parallel()
+			g := NewGomegaWithT(t)
+			r := createGlobbingRegex(c.regex)
+			match := r.MatchString(c.candidate)
+			g.Expect(match).To(Equal(c.shouldMatch))
+		})
+	}
 }
