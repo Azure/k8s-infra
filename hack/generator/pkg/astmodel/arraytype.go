bdc8ec972989f69c9966ca81c213cb965ab14415
Bevan Arps <bevan.arps@microsoft.com>
2020-05-12 17:08:55 +1200

    Implement Type.Equals() for arrays

--- hack/generator/pkg/astmodel/arraytype.go
+++ hack/generator/pkg/astmodel/arraytype.go
@@ -36,3 +36,12 @@ func (array *ArrayType) RequiredImports() []PackageReference {
 func (array *ArrayType) References(t Type) bool {
 	return array == t || array.element.References(t)
 }
+
+// Equals returns true if the passed type is an array type with the same kind of elements, false otherwise
+func (array *ArrayType) Equals(t Type) bool {
+	if et, ok := t.(*ArrayType);ok {
+		return array.element.Equals(et.element)
+	}
+
+	return false
+}
