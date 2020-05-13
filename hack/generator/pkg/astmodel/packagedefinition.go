3cf974a92a6797077dbed35da09c90045835b903
Bevan Arps <bevan.arps@microsoft.com>
2020-05-14 10:40:45 +1200

    Code gardening

--- hack/generator/pkg/astmodel/packagedefinition.go
+++ hack/generator/pkg/astmodel/packagedefinition.go
@@ -78,20 +78,6 @@ func partitionDefinitions(definitions []Definition) (resourceStructs []*StructDe
 	return resources, notResources
 }
 
-/*
-TODO: Fix allocation to files so it is deterministic
-
-Get all files referencing the type
-If more than one, put it in its own file
-
-Get all outstanding references to the type
-If no outstanding references
-	If referenced by just one file, put in that file
-	otherwise put into its own file
-
-But how to handle cycles?
-*/
-
 func allocateTypesToFiles(typesToAllocate []Definition, filesToGenerate map[string][]Definition) {
 	// Keep track of how many types we've checked since we last allocated one
 	// This lets us detect if/when we stall during allocation
