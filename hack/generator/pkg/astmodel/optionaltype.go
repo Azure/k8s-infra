1da96cd3066f9be383b00099cc3e8f8fb1802bae
George Pollard <gpollard@microsoft.com>
2020-05-15 10:36:01 +1200

    Add OptionalType to wrap maybe-present items

---
+++ hack/generator/pkg/astmodel/optionaltype.go
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) Microsoft Corporation.
+ * Licensed under the MIT license.
+ */
+
+package astmodel
+
+import (
+	"go/ast"
+)
+
+// OptionalType is used for items that may or may not be present
+type OptionalType struct {
+	element Type
+}
+
+// NewOptionalType creates a new optional type that may or may not have the specified 'element' type
+func NewOptionalType(element Type) *OptionalType {
+	return &OptionalType{element}
+}
+
+// assert we implemented Type correctly
+var _ Type = (*OptionalType)(nil)
+
+// AsType renders the Go abstract syntax tree for an array type
+func (optional *OptionalType) AsType() ast.Expr {
+	return &ast.StarExpr{
+		X: optional.element.AsType(),
+	}
+}
+
+func (optional *OptionalType) RequiredImports() []PackageReference {
+	return optional.element.RequiredImports()
+}
+
+func (optional *OptionalType) References(t Type) bool {
+	return optional == t || optional.element.References(t)
+}
