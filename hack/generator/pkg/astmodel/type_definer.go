ce6dbf4038a76312d87a2296743786bc9d7cc824
Matthew Christopher <matthchr@microsoft.com>
2020-05-24 17:48:02 -0700

    Generate snake_case filenames (#113)

--- hack/generator/pkg/astmodel/type_definer.go
+++ hack/generator/pkg/astmodel/type_definer.go
@@ -28,5 +28,5 @@ type TypeDefiner interface {
 // FileNameHint returns what a file that contains this definition (if any) should be called
 // this is not always used as we might combine multiple definitions into one file
 func FileNameHint(def TypeDefiner) string {
-	return def.Name().name
+	return transformToSnakeCase(def.Name().name)
 }
