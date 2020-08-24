e422bb8ebfffebbadde273a12aa8c6ca452fad02
Bevan Arps <bevan.arps@microsoft.com>
2020-08-25 08:32:18 +1200

    Only test Windows paths on Windows

--- hack/generator/pkg/codegen/pipeline_augment_status_test.go
+++ hack/generator/pkg/codegen/pipeline_augment_status_test.go
@@ -7,30 +7,47 @@ package codegen
 
 import (
 	. "github.com/onsi/gomega"
-
+	"runtime"
 	"testing"
 )
 
 func Test_ShouldSkipDir_GivenPath_HasExpectedResult(t *testing.T) {
-	cases := []struct {
+	linuxCases := []struct {
 		name       string
 		path       string
 		shouldSkip bool
 	}{
 		// Simple paths
 		{"Root", "/", false},
-		{"Drive", "D:\\", false},
 		{"Top level", "/foo/", false},
-		{"Top level, Windows", "D:\\foo\\", false},
 		{"Nested", "/foo/bar/", false},
-		{"Nested, Windows", "D:\\foo\\bar\\", false},
 		// Paths to skip
 		{"Skip top level", "/examples/", true},
-		{"Skip top level, Windows", "D:\\examples\\", true},
 		{"Skip nested", "/foo/examples/", true},
-		{"Skip nested, Windows", "D:\\foo\\examples\\", true},
 		{"Skip nested, trailing directory", "/foo/examples/bar/", true},
+	}
+
+	windowsCases := []struct {
+		name       string
+		path       string
+		shouldSkip bool
+	}{
+		// Simple paths
+		{"Drive", "D:\\", false},
+		{"Top level, Windows", "D:\\foo\\", false},
+		{"Nested, Windows", "D:\\foo\\bar\\", false},
+		// Paths to skip
+		{"Skip top level, Windows", "D:\\examples\\", true},
+		{"Skip nested, Windows", "D:\\foo\\examples\\", true},
 		{"Skip nested, trailing directory, Windows", "D:\\foo\\examples\\bar\\", true},
+	}
+
+	cases := linuxCases
+
+	// If testing on Windows, also test Windows paths
+	// Can't test Windows paths on Linux because *reasons*
+	if runtime.GOOS == "windows" {
+		cases = append(cases, windowsCases...)
 	}
 
 	for _, c := range cases {
