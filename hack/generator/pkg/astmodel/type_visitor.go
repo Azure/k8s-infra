bada7139fe85d1e0b6c7a3db88e93714434901f6
Bevan Arps <bevan.arps@microsoft.com>
2020-08-31 08:38:23 +1200

    Modify Type Visitor to return errors

--- hack/generator/pkg/astmodel/type_visitor.go
+++ hack/generator/pkg/astmodel/type_visitor.go
@@ -5,25 +5,30 @@
 
 package astmodel
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/pkg/errors"
+
+	kerrors "k8s.io/apimachinery/pkg/util/errors"
+)
 
 // TypeVisitor represents a visitor for a tree of types.
 // The `ctx` argument can be used to “smuggle” additional data down the call-chain.
 type TypeVisitor struct {
-	VisitTypeName     func(this *TypeVisitor, it TypeName, ctx interface{}) Type
-	VisitArrayType    func(this *TypeVisitor, it *ArrayType, ctx interface{}) Type
-	VisitPrimitive    func(this *TypeVisitor, it *PrimitiveType, ctx interface{}) Type
-	VisitObjectType   func(this *TypeVisitor, it *ObjectType, ctx interface{}) Type
-	VisitMapType      func(this *TypeVisitor, it *MapType, ctx interface{}) Type
-	VisitOptionalType func(this *TypeVisitor, it *OptionalType, ctx interface{}) Type
-	VisitEnumType     func(this *TypeVisitor, it *EnumType, ctx interface{}) Type
-	VisitResourceType func(this *TypeVisitor, it *ResourceType, ctx interface{}) Type
+	VisitTypeName     func(this *TypeVisitor, it TypeName, ctx interface{}) (Type, error)
+	VisitArrayType    func(this *TypeVisitor, it *ArrayType, ctx interface{}) (Type, error)
+	VisitPrimitive    func(this *TypeVisitor, it *PrimitiveType, ctx interface{}) (Type, error)
+	VisitObjectType   func(this *TypeVisitor, it *ObjectType, ctx interface{}) (Type, error)
+	VisitMapType      func(this *TypeVisitor, it *MapType, ctx interface{}) (Type, error)
+	VisitOptionalType func(this *TypeVisitor, it *OptionalType, ctx interface{}) (Type, error)
+	VisitEnumType     func(this *TypeVisitor, it *EnumType, ctx interface{}) (Type, error)
+	VisitResourceType func(this *TypeVisitor, it *ResourceType, ctx interface{}) (Type, error)
 }
 
 // Visit invokes the appropriate VisitX on TypeVisitor
-func (tv *TypeVisitor) Visit(t Type, ctx interface{}) Type {
+func (tv *TypeVisitor) Visit(t Type, ctx interface{}) (Type, error) {
 	if t == nil {
-		return nil
+		return nil, nil
 	}
 
 	switch it := t.(type) {
@@ -50,10 +55,24 @@ func (tv *TypeVisitor) Visit(t Type, ctx interface{}) Type {
 
 // VisitDefinition invokes the TypeVisitor on both the name and type of the definition
 // NB: this is only valid if VisitTypeName returns a TypeName and not generally a Type
-func (tv *TypeVisitor) VisitDefinition(td TypeDefinition, ctx interface{}) TypeDefinition {
-	return MakeTypeDefinition(
-		tv.VisitTypeName(tv, td.Name(), ctx).(TypeName),
-		tv.Visit(td.Type(), ctx))
+func (tv *TypeVisitor) VisitDefinition(td TypeDefinition, ctx interface{}) (*TypeDefinition, error) {
+	visitedName, err := tv.VisitTypeName(tv, td.Name(), ctx)
+	if err != nil {
+		return nil, errors.Wrapf(err, "visit of %q failed", td.Name())
+	}
+
+	name, ok := visitedName.(TypeName)
+	if !ok {
+		return nil, errors.Errorf("expected visit of %q to return TypeName, not %T", td.Name(), visitedName)
+	}
+
+	visitedType, err := tv.Visit(td.Type(), ctx)
+	if err != nil {
+		return nil, errors.Wrapf(err, "visit of type of %q failed", td.Name())
+	}
+
+	def := MakeTypeDefinition(name, visitedType)
+	return &def, nil
 }
 
 // MakeTypeVisitor returns a default (identity transform) visitor, which
@@ -62,43 +81,79 @@ func (tv *TypeVisitor) VisitDefinition(td TypeDefinition, ctx interface{}) TypeD
 func MakeTypeVisitor() TypeVisitor {
 	// TODO [performance]: we can do reference comparisons on the results of
 	// recursive invocations of Visit to avoid having to rebuild the tree if the
-	// leafs do not actually change.
+	// leaf nodes do not actually change.
 	return TypeVisitor{
-		VisitTypeName: func(_ *TypeVisitor, it TypeName, _ interface{}) Type {
-			return it
+		VisitTypeName: func(_ *TypeVisitor, it TypeName, _ interface{}) (Type, error) {
+			return it, nil
 		},
-		VisitArrayType: func(this *TypeVisitor, it *ArrayType, ctx interface{}) Type {
-			newElement := this.Visit(it.element, ctx)
-			return NewArrayType(newElement)
+		VisitArrayType: func(this *TypeVisitor, it *ArrayType, ctx interface{}) (Type, error) {
+			newElement, err := this.Visit(it.element, ctx)
+			if err != nil {
+				return nil, errors.Wrap(err, "failed to visit type of array")
+			}
+
+			return NewArrayType(newElement), nil
 		},
-		VisitPrimitive: func(_ *TypeVisitor, it *PrimitiveType, _ interface{}) Type {
-			return it
+		VisitPrimitive: func(_ *TypeVisitor, it *PrimitiveType, _ interface{}) (Type, error) {
+			return it, nil
 		},
-		VisitObjectType: func(this *TypeVisitor, it *ObjectType, ctx interface{}) Type {
+		VisitObjectType: func(this *TypeVisitor, it *ObjectType, ctx interface{}) (Type, error) {
 			// just map the property types
+			var errs []error
 			var newProps []*PropertyDefinition
 			for _, prop := range it.properties {
-				newProps = append(newProps, prop.WithType(this.Visit(prop.propertyType, ctx)))
+				p, err := this.Visit(prop.propertyType, ctx)
+				if err != nil {
+					errs = append(errs, err)
+				} else {
+					newProps = append(newProps, prop.WithType(p))
+				}
 			}
-			return it.WithProperties(newProps...)
+
+			if len(errs) > 0 {
+				return nil, kerrors.NewAggregate(errs)
+			}
+
+			return it.WithProperties(newProps...), nil
 		},
-		VisitMapType: func(this *TypeVisitor, it *MapType, ctx interface{}) Type {
-			newKey := this.Visit(it.key, ctx)
-			newValue := this.Visit(it.value, ctx)
-			return NewMapType(newKey, newValue)
+		VisitMapType: func(this *TypeVisitor, it *MapType, ctx interface{}) (Type, error) {
+			visitedKey, err := this.Visit(it.key, ctx)
+			if err != nil {
+				return nil, errors.Wrapf(err, "failed to visit map key type %v", it.key)
+			}
+
+			visitedValue, err := this.Visit(it.value, ctx)
+			if err != nil {
+				return nil, errors.Wrapf(err, "failed to visit map value type %v", it.value)
+			}
+
+			return NewMapType(visitedKey, visitedValue), nil
 		},
-		VisitEnumType: func(_ *TypeVisitor, it *EnumType, _ interface{}) Type {
+		VisitEnumType: func(_ *TypeVisitor, it *EnumType, _ interface{}) (Type, error) {
 			// if we visit the enum base type then we will also have to do something
 			// about the values. so by default don't do anything with the enum base
-			return it
+			return it, nil
 		},
-		VisitOptionalType: func(this *TypeVisitor, it *OptionalType, ctx interface{}) Type {
-			return NewOptionalType(this.Visit(it.element, ctx))
+		VisitOptionalType: func(this *TypeVisitor, it *OptionalType, ctx interface{}) (Type, error) {
+			visitedElement, err := this.Visit(it.element, ctx)
+			if err != nil {
+				return nil, errors.Wrapf(err, "failed to visit optional element type %v", it.element)
+			}
+
+			return NewOptionalType(visitedElement), nil
 		},
-		VisitResourceType: func(this *TypeVisitor, it *ResourceType, ctx interface{}) Type {
-			spec := this.Visit(it.spec, ctx)
-			status := this.Visit(it.status, ctx)
-			return NewResourceType(spec, status).WithOwner(it.Owner())
+		VisitResourceType: func(this *TypeVisitor, it *ResourceType, ctx interface{}) (Type, error) {
+			visitedSpec, err := this.Visit(it.spec, ctx)
+			if err != nil {
+				return nil, errors.Wrapf(err, "failed to visit resource spec type %v", it.spec)
+			}
+
+			visitedStatus, err := this.Visit(it.status, ctx)
+			if err != nil {
+				return nil, errors.Wrapf(err, "failed to visit resource status type %v", it.status)
+			}
+
+			return NewResourceType(visitedSpec, visitedStatus).WithOwner(it.Owner()), nil
 		},
 	}
 }
