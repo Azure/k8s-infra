4e7f11ef95a1d2c8d60b762bf42a94aeb1a3e492
Bevan Arps <bevan.arps@microsoft.com>
2020-10-02 14:48:25 +1300

    Define the TestCase interface

---
+++ hack/generator/pkg/astmodel/test_case.go
@@ -0,0 +1,24 @@
+package astmodel
+
+import "go/ast"
+
+// TestCase represents a test we generate to ensure the generated code works as expected
+type TestCase interface {
+	// Name returns the unique name of this test case
+	Name() string
+
+	// References returns the set of types to which this test case refers.
+	// Should *not* include the receiver of this function
+	References() TypeNameSet
+
+	// RequiredImports returns a set of the package imports required by this test case
+	RequiredImports() *PackageImportSet
+
+	// AsFuncs renders the current test case and any supporting methods as Go abstract syntax trees
+	// subject is the name of the type under test
+	// codeGenerationContext contains reference material to use when generating
+	AsFuncs(subject TypeName, codeGenerationContext *CodeGenerationContext) []ast.Decl
+
+	// Equals determines if this TestCase is equal to another one
+	Equals(f TestCase) bool
+}
