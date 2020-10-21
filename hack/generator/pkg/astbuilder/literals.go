d31f7572fed274f344f58d667d0b020c28cf84e4
Bevan Arps <bevan.arps@microsoft.com>
2020-10-22 11:24:21 +1300

    Create literals support helper

--- hack/generator/pkg/astbuilder/literals.go
+++ hack/generator/pkg/astbuilder/literals.go
@@ -1,1 +1,13 @@
 package astbuilder
+
+import (
+	"go/ast"
+	"go/token"
+)
+
+func LiteralString(content string) ast.Expr {
+	return &ast.BasicLit{
+		Value: "\"" + content + "\"",
+		Kind:  token.STRING,
+	}
+}
