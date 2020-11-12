78ef145136f80b00d90ec6bb5a740abc18b4cfd3
Bevan Arps <bevan.arps@microsoft.com>
2020-11-12 14:41:34 +1300

    Add unit tests for current pipeline content

---
+++ hack/generator/pkg/codegen/pipeline_factory_test.go
@@ -0,0 +1,62 @@
+package codegen
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/Azure/k8s-infra/hack/generator/pkg/astmodel"
+	"github.com/Azure/k8s-infra/hack/generator/pkg/config"
+	"github.com/sebdah/goldie/v2"
+	"strings"
+	"testing"
+
+	. "github.com/onsi/gomega"
+)
+
+func TestNewArmCodeGeneratorFromConfigCreatesRightPipeline(t *testing.T) {
+	gold := goldie.New(t)
+	g := NewGomegaWithT(t)
+
+	idFactory := astmodel.NewIdentifierFactory()
+	configuration := config.NewConfiguration()
+	codegen, err := NewArmCodeGeneratorFromConfig(configuration, idFactory)
+	g.Expect(err).To(BeNil())
+
+	result := writePipeline("Expected Pipeline Stages for ARM Code Generation", codegen)
+
+	gold.Assert(t, "ARMCodeGeneratorPipeline", result)
+}
+
+func TestNewTestCodeGeneratorCreatesRightPipeline(t *testing.T) {
+	gold := goldie.New(t)
+	g := NewGomegaWithT(t)
+
+	config := makeDefaultTestConfig()
+	codegen, err := NewTestCodeGenerator("Sample", "path", t, config)
+	g.Expect(err).To(BeNil())
+
+	result := writePipeline("Expected Pipeline Stages for Test Code Generation", codegen)
+
+	gold.Assert(t, "TestCodeGeneratorPipeline", result)
+}
+
+func writePipeline(title string, codegen *CodeGenerator) []byte {
+	var b bytes.Buffer
+
+	fmt.Fprintln(&b, title)
+	fmt.Fprintln(&b, strings.Repeat("-", len(title)))
+
+	for _, s := range codegen.pipeline {
+		targets := ""
+		for _, t := range s.targets {
+			if len(targets) > 0 {
+				targets = targets + "; "
+			}
+
+			targets = targets + t.String()
+		}
+
+		fmt.Fprintf(&b, "%-35s %-10s %s\n", s.id, targets, s.description)
+	}
+
+	return b.Bytes()
+}
