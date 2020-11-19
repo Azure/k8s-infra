65e73aa09ab0edc1fe7334b3dc4673df2dd83da2
Bevan Arps <bevan.arps@microsoft.com>
2020-11-19 14:56:45 +1300

    Add Wrap() method

--- hack/generator/pkg/astmodel/type_flag.go
+++ hack/generator/pkg/astmodel/type_flag.go
@@ -12,6 +12,12 @@ const (
 
 var _ fmt.Stringer = TypeFlag("")
 
+// String() renders the tag as a string
 func (f TypeFlag) String() string {
 	return string(f)
+}
+
+// ApplyTo() applies the tag to the provided type
+func (f TypeFlag) ApplyTo(t Type) *FlaggedType {
+	return NewFlaggedType(t, f)
 }
