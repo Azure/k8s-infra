f526a6ce0e4e71e53d1ebe269e528a66f5105709
George Pollard <gpollard@microsoft.com>
2020-11-24 01:47:09 +0000

    Fix other unit tests

--- hack/generated/pkg/util/armresourceresolver/resolver_test.go
+++ hack/generated/pkg/util/armresourceresolver/resolver_test.go
@@ -38,7 +38,8 @@ func createResourceGroup(name string) *resources.ResourceGroup {
 			Name: name,
 		},
 		Spec: resources.ResourceGroupSpec{
-			Location: "West US",
+			Location:  "West US",
+			AzureName: name, // defaulter webhook will copy Name to AzureName
 		},
 	}
 }
@@ -57,6 +58,7 @@ func createResourceGroupRootedResource(rgName string, name string) (genruntime.M
 			Owner: genruntime.KnownResourceReference{
 				Name: rgName,
 			},
+			AzureName: name, // defaulter webhook will copy Name to AzureName
 		},
 	}
 
@@ -74,6 +76,7 @@ func createDeeplyNestedResource(rgName string, parentName string, name string) R
 			Owner: genruntime.KnownResourceReference{
 				Name: rgName,
 			},
+			AzureName: parentName, // defaulter webhook will copy Name to AzureName
 		},
 	}
 
