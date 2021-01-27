b698fea3314796c289ac7982f2d4c0ffbac83db2
George Pollard <gpollard@microsoft.com>
2021-01-27 20:49:53 +0000

    Add support for HTTP dates in Retry-After

--- hack/generated/pkg/armclient/raw_client.go
+++ hack/generated/pkg/armclient/raw_client.go
@@ -159,9 +159,28 @@ func getRetryAfter(resp *http.Response) time.Duration {
 		if retryAfterVal, parseErr := strconv.ParseInt(retryAfterStr, 10, 64); parseErr == nil {
 			return time.Duration(retryAfterVal) * time.Second
 		}
+
+		if retryAfterTime, parseErr := parseHttpDate(retryAfterStr); parseErr == nil {
+			result := time.Until(retryAfterTime)
+			if result > 0 {
+				return result
+			}
+		}
 	}
 
 	return 0
+}
+
+func parseHttpDate(s string) (time.Time, error) {
+	if t, err := time.Parse("Mon, 02 Jan 2006 15:04:05 MST", s); err == nil {
+		return t, nil
+	} else if t, err = time.Parse("Monday, 02-Jan-06 15:04:05 MST", s); err == nil {
+		return t, nil
+	} else if t, err = time.Parse("Mon Jan  2 15:04:05 2006", s); err == nil {
+		return t, nil
+	}
+
+	return time.Time{}, errors.New("unable to parse date")
 }
 
 // DeleteResource will make an HTTP DELETE call to the resourceId and attempt to fill the resource with the response.
