67612ff181371aa99fbef92f157d2a758113c9ca
Bevan Arps <bevan.arps@microsoft.com>
2021-02-02 16:51:06 +1300

    Make NewEnumType varidic to simplify testing

--- hack/generator/pkg/jsonast/jsonast.go
+++ hack/generator/pkg/jsonast/jsonast.go
@@ -408,7 +408,7 @@ func enumHandler(ctx context.Context, scanner *SchemaScanner, schema Schema) (as
 		values = append(values, astmodel.EnumValue{Identifier: id, Value: v})
 	}
 
-	enumType := astmodel.NewEnumType(baseType, values)
+	enumType := astmodel.NewEnumType(baseType, values...)
 
 	return enumType, nil
 }
