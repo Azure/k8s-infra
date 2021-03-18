2e2539ea4b4da68286e011615bf29d9776fad008
Bevan Arps <bevan.arps@microsoft.com>
2021-03-19 12:09:36 +1300

    Simplify implementation

--- hack/generated/pkg/util/banner_logger.go
+++ hack/generated/pkg/util/banner_logger.go
@@ -22,17 +22,17 @@ func NewBannerLogger() *BannerLogger {
 }
 
 func (b *BannerLogger) Header(content string) {
-	b.WriteBanner("=", fmt.Sprintf("%s %s", b.label(), content))
+	lineLength := utf8.RuneCountInString(content)
+	fmt.Println(strings.Repeat("=", lineLength))
+	fmt.Println(content)
+	fmt.Println(strings.Repeat(line, lineLength))
 }
 
 func (b *BannerLogger) Subheader(content string) {
-	b.WriteBanner("-", fmt.Sprintf("%s %s", b.label(), content))
-}
-
-func (b *BannerLogger) WriteBanner(line string, content string) {
-	lineLength := utf8.RuneCountInString(content)
+	text := fmt.Sprintf("%s %s", b.label(), content)
+	lineLength := utf8.RuneCountInString(text)
 	fmt.Println(strings.Repeat(line, lineLength))
-	fmt.Println(content)
+	fmt.Println(text)
 	fmt.Println(strings.Repeat(line, lineLength))
 }
 
