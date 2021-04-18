665809053af0b6e8ea4167dbebf9f18473031917
Bevan Arps <bevan.arps@microsoft.com>
2021-04-19 11:55:27 +1200

    Use discards where useful

--- hack/generator/pkg/astmodel/optional_type.go
+++ hack/generator/pkg/astmodel/optional_type.go
@@ -67,7 +67,7 @@ func (optional *OptionalType) AsType(codeGenerationContext *CodeGenerationContex
 
 // AsZero renders an expression for the "zero" value of the type
 // by returning a literal "nil"
-func (optional *OptionalType) AsZero(types Types, ctx *CodeGenerationContext) dst.Expr {
+func (optional *OptionalType) AsZero(_ Types, _ *CodeGenerationContext) dst.Expr {
 	return &dst.BasicLit{
 		Value: "nil",
 	}
